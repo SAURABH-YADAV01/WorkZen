972f1508d69ce63370e1749404bd91f507585b2b
Saurabh <saurabhyadav12207@gmail.com>
2026-05-07 11:43:36 +0530

    viteconfig

--- frontend/src/vite.config.js
+++ frontend/src/vite.config.js
@@ -3,6 +3,14 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   server: {
     host: '0.0.0.0',
-    allowedHosts: ['.railway.app']
+    allowedHosts: [
+      'successful-gentleness-production-5a49.up.railway.app'
+    ]
+  },
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: [
+      'successful-gentleness-production-5a49.up.railway.app'
+    ]
   }
 })
